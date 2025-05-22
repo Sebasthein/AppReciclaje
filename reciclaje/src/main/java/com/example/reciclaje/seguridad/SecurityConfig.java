357793276c1aa4925ab3c79da618df99aa9b32db
Sebasthein <sebasatiangudi234@gmail.com>
2025-05-22 00:12:35 -0500

    Lector de QR
    Implementacion de lectura de codigo QR en dashboard

--- reciclaje/src/main/java/com/example/reciclaje/seguridad/SecurityConfig.java
+++ reciclaje/src/main/java/com/example/reciclaje/seguridad/SecurityConfig.java
@@ -10,17 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
 import com.example.reciclaje.servicio.UserDetailsServiceImpl;
+
 
 @Configuration
 @EnableMethodSecurity
@@ -54,7 +45,7 @@ public class SecurityConfig {
 	        http
 	            .csrf(csrf -> csrf.disable())
 	            .authorizeHttpRequests(auth -> auth
-	                .requestMatchers("/login", "/registro","/api/**","/api/usuarios/registro", "/css/**", "/js/**","/img/**").permitAll() // Permitir acceso a estas rutas
+	                .requestMatchers("/login", "/registro","/api/**","/api/usuarios/registro", "/css/**", "/js/**","/img/**", "/plugins/**").permitAll() // Permitir acceso a estas rutas
 	                .requestMatchers("/admin/**").hasRole("ADMIN")
 	                .anyRequest().authenticated() // El resto requiere autenticación
 	            )
